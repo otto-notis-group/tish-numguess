e2f5b8185a9f73d814b2c92002b79ed17271cc31
mixueicecreamandtea <qwertyuiop9296@outlook.com>
2024-05-01 22:04:35 +0800

    Create fun.py

---
+++ fun.py
@@ -0,0 +1,46 @@
+#numguess
+import requests as rq
+import os
+import random as rn
+def genrate():
+    switch=rn.choice([1,2,3,4,5,6,7,8,9])
+    print(switch)
+    if switch==1:
+        import time as tm
+        return float(tm.strftime("%Y%H%M%S%S%Y"))+rn.uniform(1.0,0.3),"Tip:Current time (format:%Y%H%M%S%S%Y) + a random float between 0 and 1"
+    elif switch==2:
+        import tempfile
+        return float(os.path.getsize(tempfile.gettempdir())+len(rq.get(rn.choice(["https://example.com","https://python.org"])).content)),"Tip:Size of temp directory + length(in bytes) of https://example.com/ or https://python.org/"
+    elif switch==3:
+        return float(int(rn.choice(["US","CN","JP","IN","GB","RU"])+rn.choice(["US","CN","JP","IN","GB","RU"]),36)+rn.uniform(1.0,0.3)),"Tip:Base 36 sum of 2 country codes + a random float between 0 and 1"
+    elif switch==4:
+        import sys
+        return rn.uniform(sys.float_info.max,sys.float_info.min),"Tip:Random float between sys.float_info.max and sys.float_info.min"
+    else:return genrate()
+def numguess(n:float,target: float):
+    rtr=""
+    if n>target:
+        rtr+="Too high"
+    else:rtr+="Too low"
+    if len(str(int(n)))>len(str(int(target))):
+        rtr+= " and too long on int part"
+    elif len(str(int(n)))<len(str(int(target))):
+        rtr+= " and too short on int part"
+    else:rtr+=" and perfect on int part"
+    if len(str(n).split(".")[1])>len(str(target).split(".")[1]):
+        rtr+= " and too long on float part"
+    elif len(str(n).split(".")[1])<len(str(target).split(".")[1]):
+        rtr+= " and too short on float part"
+    else:rtr+=" and perfect on float part"
+    return rtr
+print("Welcome to the number guessing game!")
+while True:
+    tgt,tip=genrate()
+    rrst=True
+    while rrst:
+        print(tip)
+        rst=input("Guess the number: ")
+        rrst=numguess(float(rst),tgt)
+        print(rrst)
+
+    
